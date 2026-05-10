66f94f362c5b7f90ff6a66d4677efaeec66af297
Rafael Gustavo <88495051+RafaelGustavo45@users.noreply.github.com>
2026-05-10 10:07:59 -0300

    adição dos arquivos do blog

---
+++ projeto_blog/src/app/data/dataFake.ts
@@ -0,0 +1,36 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "FUI BANIDO NO SOUL FANFICS",
+    "resumo": "Banimentos estranhos por tras daquela moderação, o que aconteceu? O que eu fiz? O que eu não fiz? Descubra tudo aqui",
+    "description": "Banimentos estranhos por tras daquela moderação, o que aconteceu? O que eu fiz? O que eu não fiz? Descubra tudo aqui, O que é o Soul FanFics? O Soul FanFics é uma plataforma de leitura e escrita de fanfics, onde os usuários podem criar e compartilhar suas histórias baseadas em personagens e universos de obras de ficção. A plataforma é popular entre os fãs de diversas franquias, como animes, séries, filmes e jogos. Os usuários podem interagir com as histórias, deixando comentários e avaliações, além de seguir seus autores favoritos. O Soul FanFics é um espaço para a criatividade e a expressão dos fãs, permitindo que eles explorem suas paixões por meio da escrita e da leitura de fanfics. Porem o que acabou acontecendo foi o seguinte, lá estava eu escrevendo uma fanfic, quando de repente, sem motivo aparente, recebi um aviso de banimento. Fiquei completamente surpreso e confuso, pois não tinha feito nada que violasse as regras da plataforma. Tentei entrar em contato com a equipe de suporte para entender o motivo do banimento, mas não obtive resposta. Foi uma experiência frustrante e decepcionante, pois eu gostava de compartilhar minhas histórias com outros fãs e agora me sentia injustiçado. O que aconteceu? O que eu fiz? O que eu não fiz? Essas perguntas ficaram sem resposta, deixando-me com um sentimento de injustiça e frustração em relação ao Soul FanFics. Eu até futuramente consegui uma resposta, mas parece que foi fruto de uma má interpretação de um trecho de uma historia que escrevi afirmando ser apologia, mesmo escrevendo neutramente de forma a narrar o fato",
+    "photoCover":"https://i.pinimg.com/736x/cb/b7/94/cbb79470fc1f571618700348795d3f1f.jpg",
+    "isMain": true
+  },
+  {
+    "id":"2",
+    "title": "ARTIGO ESTRANHO DO FOTON VISTO EM 7 DIMENSÕES",
+    "resumo": "Recentemente saiu na matéria um emperimento sobre um foton visto em 7 dimensões espaciais, mas ai tem algo estranho",
+    "description": "Recentemente saiu na matéria um emperimento sobre um foton visto em 7 dimensões espaciais, mas ai tem algo estranho, a mesma ciencia que afirma que o cerebro humano só pode compreender 4 dimensões, são 3 de espaço e 1 de tempo afirma indiretamente por consequencia lógica que teve gente que conseguiu tal feito neste experimento ",
+    "photoCover":"https://img.odcdn.com.br/wp-content/uploads/2024/06/shutterstock_2415018859-1920x1080.jpg",
+    "isMain": false
+
+  },
+  {
+    "id": "3",
+    "title": "REDUZ UM PARA AUMENTAR O OUTRO",
+    "resumo": "nossa tecnologia parece estar em um falso desenvolvimento, onde para aumentar um aspecto, outro é reduzido, o que pode ser perigoso",
+    "description": "nossa tecnologia parece estar em um falso desenvolvimento, onde para aumentar um aspecto, outro é reduzido, o que pode ser perigoso, por exemplo, a cada nova geração de celular, temos uma bateria que dura menos, ou seja, para termos um celular mais potente, temos uma bateria pior, isso é um exemplo claro de como nossa tecnologia parece estar em um falso desenvolvimento, onde para aumentar um aspecto, outro é reduzido, o que pode ser perigoso. Um exemplo disso são os fones de ouvido sem fio, não é tecnologia superior mas venderam se fossem, pois resolveram o problema do emaranhamento de fios ao remover mas criou a necessidade de ficar recarregando o tempo todo e ainda por cima não resolveram o problema do vicio das baterias",
+    "photoCover":"https://fastshopbr.vtexassets.com/arquivos/ids/4470804/0_0_67dcb5ad2954dd4c58245531.jpg?v=639104985962800000",
+    "isMain": false
+  },
+
+  {
+    "id": "4",
+    "title": "CONTROLE PELA HISTORIA",
+    "resumo": "Uma teoria da conspiração surgiu dizendo que autores do passado estão controlando o presente para bater com suas historias",
+    "description": "Isso tudo começou com o livro 1984, na qual se ve diversos governos fazendo cosplay do que é mostrado no livro, o que foi dito está literalmente acontecendo, governos estão controlando a população por meio do medo, da manipulação e da censura, isso é algo que já foi mostrado em diversos livros, filmes e séries, mas agora parece que estamos vivendo isso na vida real, o que é assustador e preocupante, pois mostra que a ficção pode se tornar realidade, e que os autores do passado podem estar controlando o presente para bater com suas historias",
+    "photoCover":"https://atlantico.org.br/wp-content/uploads/2025/02/george-orwell-1984-tv-show-wiip-1252214_widelg.jpeg",
+    "isMain": false
+  }
+]
